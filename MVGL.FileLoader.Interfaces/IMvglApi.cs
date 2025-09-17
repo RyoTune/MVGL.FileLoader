2a64d5330ed4080d450c8efa30db261d4938a7e1
RyoTune <zutomayofan50@gmail.com>
2025-09-16 20:30:21 -0500

    Add docs.

--- MVGL.FileLoader.Interfaces/IMvglApi.cs
+++ MVGL.FileLoader.Interfaces/IMvglApi.cs
@@ -1,8 +1,21 @@
 namespace MVGL.FileLoader.Interfaces;
 
+/// <summary>
+/// MVGL File Loader API.
+/// </summary>
 public interface IMvglApi
 {
+    /// <summary>
+    /// Add a folder to load files from.
+    /// </summary>
+    /// <param name="folder">Folder path.</param>
+    /// <returns>Amount of files loaded.</returns>
     int AddFolder(string folder);
 
+    /// <summary>
+    /// Bind a file to a path.
+    /// </summary>
+    /// <param name="bindPath">Path to bind.</param>
+    /// <param name="file">File being bound.</param>
     void BindFile(string bindPath, string file);
 }
