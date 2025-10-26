221610fb72b718d4b13416043b5e610d3fd4a562
RyoTune <zutomayofan50@gmail.com>
2025-10-26 17:06:40 -0500

    Implement file processors in registry.

--- MVGL.FileLoader.Reloaded/MvglModRegistry.cs
+++ MVGL.FileLoader.Reloaded/MvglModRegistry.cs
@@ -1,19 +1,34 @@
 using System.Diagnostics.CodeAnalysis;
+using MVGL.FileLoader.Interfaces.FileProcessors;
 
 namespace MVGL.FileLoader.Reloaded;
 
 public class MvglModRegistry
 {
     private readonly Dictionary<string, string> _mvglFiles = new(StringComparer.OrdinalIgnoreCase);
+    private readonly Dictionary<IFileProcessor, List<HandledFile>> _processors = [];
 
     public int AddFolder(string folder)
     {
         var numFiles = 0;
         foreach (var file in Directory.EnumerateFiles(folder, "*", SearchOption.AllDirectories))
         {
-            var relPath = Path.GetRelativePath(folder, file).Replace('\\', '/');
-            BindFile(relPath, file);
             numFiles++;
+
+            var relPath = Path.GetRelativePath(folder, file);
+
+            var isHandled = false;
+            foreach (var proc in _processors)
+            {
+                if (!proc.Key.CanHandle(file)) continue;
+                proc.Value.Add(new() { InitialBindPath = relPath, FilePath = file });
+                isHandled = true;
+                Log.Debug($"File handled by processor '{proc.Key.GetType().Name}'.\nFile: {file}");
+                break;
+            }
+
+            if (isHandled) continue;
+            BindFile(relPath, file);
         }
 
         return numFiles;
@@ -34,5 +49,13 @@ public class MvglModRegistry
             _mvglFiles.TryGetValue(Path.ChangeExtension(gameFile, ".dds"), out modFile)) return true;
         
         return false;
+    }
+
+    public void RegisterProcessor(IFileProcessor processor) => _processors[processor] = [];
+
+    public void ProcessFiles()
+    {
+        foreach (var kvp in _processors)
+            kvp.Key.Process(kvp.Value);
     }
 }
